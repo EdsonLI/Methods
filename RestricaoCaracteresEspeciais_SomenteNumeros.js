d4d1aba9071f7e5ff428ac1d6527fbcdd5e1bde3
unknown <edson.isele@marketing01.intranet.cielo.ind.br>
2016-03-30 11:30:50 -0300

    Metodo para suprimir a entrada de caracteres especificos atraves do teclado

---
+++ RestricaoCaracteresEspeciais_SomenteNumeros.js
@@ -0,0 +1,34 @@
+<script>   
+    /* define a acao iniciada pelo evento keydown relacionado ao seletor, nesse caso o input com a propriedade name igual a cbxTempoPadrao */
+    $('input[name=cbxTempoPadrao]').keydown(function(e) {
+        //console.info('chamou keydown'+e.which);
+        var $this = $(this), /* armazena o ponteiro em uma variavel e mantem uma copia original */
+            teclas = [188, 110, 190, 194, 107, 109, 187, 189, 69]; /* define as teclas que serao bloqueadas para esse input */
+        
+        prevntNumeric(e, $this, teclas); /* faz a chamada do metodo */
+    });
+
+    /* define a acao iniciada pelo evento keydown relacionado ao seletor, nesse caso o input com a propriedade name igual a cbxEntradaInstalacao */
+    $('input[name=cbxEntradaInstalacao]').keydown(function(e) {
+        //console.info('chamou keydown'+e.which);
+        var $this = $(this), /* armazena o ponteiro em uma variavel e mantem uma copia original */
+            teclas = [188, 110, 190, 194, 107, 109, 187, 189, 69]; /* define as teclas que serao bloqueadas para esse input */
+        
+        prevntNumeric(e, $this, teclas); /* faz a chamada do metodo */
+    });
+    /* metodo para previnir/suprimir a entrada de caracteres especificos atraves do teclado */
+    function prevntNumeric(event, seletor, teclas) {
+        var val = seletor.val();
+        if(val.length > 1) {
+            event.preventDefault();
+            val.substring(0,(val.length - 1));
+        }
+        for(var i = 0, len = teclas.length; i < len; i++) {
+            if(event.which === teclas[i]) { /* compara se ha caracteres nao permitidos */
+                event.preventDefault();
+                val.substring(0,(val.length - 1));
+            }
+            //console.info(teclas[i]);
+        }
+    }
+</script>

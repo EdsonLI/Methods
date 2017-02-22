d1819fc139b58672e012b42d3ea0a7a6f688ffaf
Edson Luis Isele <edsonisele@outlook.com>
2017-02-22 17:21:59 -0300

    Atualizado o arquivo contendo expressoes regulares comuns na validacao de dados

--- ExpressoesRegulares.js
+++ ExpressoesRegulares.js
@@ -16,3 +16,8 @@
 		document.getElementById("demo").innerHTML = res;
 	}
 </script>
+
+/* Logic:
+	The first number (hours) is either: a number between 0 and 19 --> [0-1]?[0-9] (allowing single digit number) or a number between 20 - 23 --> 2[0-3]
+	The second number (minutes) is always a number between 00 and 59 --> [0-5][0-9] (not allowing a single digit)
+*/
